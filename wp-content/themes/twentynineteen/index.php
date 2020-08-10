49bb0d20aed64e10a9d091d327449baf4aa68ec3
Sergii Torkhov <torhov.s@gmail.com>
2020-08-10 16:54:28 +0300

    add readme

--- wp-content/themes/twentynineteen/index.php
+++ wp-content/themes/twentynineteen/index.php
@@ -1,6 +1,8 @@
 <link rel='stylesheet' href='/wp-content/themes/twentynineteen/dist/style.css'>
 <script type='text/javascript' src='/wp-content/themes/twentynineteen/dist/index.js'></script>
 
-<div class="hello">Hello!</div>
+<div class="hello">Hello - Please make page here!</div>
 
-2000-<?php echo $created_at; ?>
+<div class="footer">
+  2000-<?php echo $created_at; ?>
+</div>
