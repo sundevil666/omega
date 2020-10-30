c551957dcc6ac0acb2fe67e3e68382d334dad5ba
sundevil <sundevildi@gmail.com>
2020-10-30 11:36:57 +0200

    first commit

---
+++ wp-content/themes/twentynineteen/component/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+      app {{ msg }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'hell, Dimon'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ wp-content/themes/twentynineteen/component/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>header</header>
+</template>
+
+<script>
+export default {
+  name: "Header.vue"
+}
+</script>
+
+<style>
+
+</style>
